1acbb6a743bc7bc65910dd7a2364edf2cc0c2946
Siwar.Bouhamed <siwar.bouhamed@groupe-telnet.net>
2022-04-23 10:58:16 +0200

    SDS-111: selenium

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.select import Select
 
 options = Options()
 options.binary_location = r'C:\Users\siwar.bouhamed\AppData\Local\Mozilla Firefox\firefox.exe'
 # driver = webdriver.Firefox(executable_path="D:\\geckodriver.exe", options=options)
+driver = webdriver.Chrome(executable_path="D:\\chromedriver.exe")
 
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
 
 
 driver.maximize_window()
@@ -15,10 +17,14 @@ driver.get("http://automationpractice.com/index.php")
 print(driver.title)
 print(driver.current_url)
 
-time.sleep(5.4)
+time.sleep(2)
 
 
-driver.find_element_by_id().click()
+driver.find_element_by_id("search_query_top").send_keys("dress")
+driver.find_element_by_name("submit_search").click()
 
-time.sleep(5.4)
+select=Select(driver.find_element_by_id("selectProductSort"))
+select.select_by_value("position:asc")
+
+time.sleep(2)
 driver.close()
