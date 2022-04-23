6d60db5e98500c5360c4c58a4b4157e377993a73
pkandybowicz <pkandybowicz@sii.pl>
2022-04-23 10:32:34 +0200

    SDS-112: fixes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
@@ -5,16 +7,18 @@ options = Options()
 options.binary_location = r'C:\Users\siwar.bouhamed\AppData\Local\Mozilla Firefox\firefox.exe'
 # driver = webdriver.Firefox(executable_path="D:\\geckodriver.exe", options=options)
 
-driver = webdriver.chrome(executable_path="D:\\chromedriver.exe")
+driver = webdriver.Chrome()
 
 
 driver.maximize_window()
-driver.get("https://www.google.com/")
+driver.get("http://automationpractice.com/index.php")
 print(driver.title)
 print(driver.current_url)
-driver.get("https://stackoverflow.com/")
-print(driver.title)
-driver.minimize_window()
-driver.back()
-# driver.refresh()
+
+time.sleep(5.4)
+
+
+driver.find_element_by_id().click()
+
+time.sleep(5.4)
 driver.close()
