72bfe77d34aef7e084fa62e21d0d32b54681cbcc
Siwar.Bouhamed <siwar.bouhamed@groupe-telnet.net>
2022-04-23 10:17:25 +0200

    SDS-111: selenium

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.binary_location = r'C:\Users\siwar.bouhamed\AppData\Local\Mozilla Firefox\firefox.exe'
+# driver = webdriver.Firefox(executable_path="D:\\geckodriver.exe", options=options)
+
+driver = webdriver.chrome(executable_path="D:\\chromedriver.exe")
+
+
+driver.maximize_window()
+driver.get("https://www.google.com/")
+print(driver.title)
+print(driver.current_url)
+driver.get("https://stackoverflow.com/")
+driver.minimize_window()
+driver.back()
+# driver.refresh()
+driver.close()
